b8681a4366170e9310433b5e1ddcf83bac0e3998
Alan Mareines <alanmareines@gmail.com>
2019-10-01 18:52:30 -0300

    oi

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,15 +1,13 @@
+# :nodoc:
 class QuestionsController < ApplicationController
   # frozen_string_literal: true
-
-  def ask
-  end
+  def ask; end
 
   def answer
     @question = params[:question]
     if @question.blank?
       @question = ''
-    elsif
-      @question == 'I am going to work'
+    elsif @question == 'I am going to work'
       @answer = 'Great!'
     elsif @question.include?('?')
       @answer = 'Silly question, get dressed and go to work!'
